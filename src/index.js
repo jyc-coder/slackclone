b0f8b1e212af02533e51eeb720d234514bbb2306
jyc-coder <jyc4648@gmail.com>
2022-10-28 21:32:14 +0900

    #7 - 회원가입 reducer 구현
    * 회원가입 양식 작성시 user값을 지정하는 액션 타입과 현재 유저 값을 제거하는 액션 타입을 작성함
    
    * 로그아웃을 했을 때 현재 유저 정보를 지워서 로그아웃이 되었다는 상태를 표시하기 위함

--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { legacy_createStore } from 'redux';
+import rootReducer from './store/index';
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={legacy_createStore(rootReducer)}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
