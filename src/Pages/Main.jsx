7c141062d25b73302c926077de9b4d21382b8269
jyc-coder <jyc4648@gmail.com>
2022-10-29 20:07:39 +0900

    #13 - 채널 메뉴 UI 구현
    * `Main`컴포넌트에  `Drawer`컴포넌트를 추가해서 채널 목록과 채널 추가 버튼을 구현함
    
    
    * `Drawer`의 설정을 `permanent`로 작성하여 지속적으로 페이지에서 볼 수 있게 구현하고 하위 목록 은 `ChannelMenu` 컴포넌트를 따로 작성해서 구현함
    
    * `ChannelMenu`는 `List`컴포넌트를 사용해서 `ListItem`에  채널 추가 버튼인 `AddIcon`과 좌측에 `채널`이라고 적힌 `ItemIcon`으로 표시되며 추가 버튼을 클릭하면 `Dialog`가 나타나서 사용자가 채널 이름과 설명을 적고 `생성`버튼을 누르면 채널이 추가가 되는 방식으로 구현함 ( 생성기능은 아직 구현하지 않았음)

--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import { Box } from '@mui/system';
 import Header from '../components/Header';
+import { Drawer, Toolbar } from '@mui/material';
+import ChannelMenu from '../components/ChannelMenu';
 
 function Main() {
     return (
       //TODO backgroundColor 테마 적용
       <Box sx={{display:"flex", backgroundColor:'white'}}>
-          <Header></Header>
+            <Header></Header>
+            <Drawer variant="permanent" sx={{width: 300}} className="no-scroll">
+                <Toolbar />
+                <Box sx={{ display: "flex", minHeight: "calc(100vh - 64px)" }}>
+                   <ChannelMenu></ChannelMenu>
+                </Box>
+            </Drawer>
     </Box>
   )
 }
