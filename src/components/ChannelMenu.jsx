fc3e6a5dfbb9a33b145e41a25a079e6dd378c82d
jyc-coder <jyc4648@gmail.com>
2022-10-29 21:55:04 +0900

    #15 - 채널 메뉴 기능 구현( 채널 추가 )
    * 채널 추가 버튼을 클릭하여 나타난 `Dialog`에 채널 이름과 설명을 입력하고 생성 버튼을 누르면 `handleSubmit`을 호출하여 채널 목록에 추가되는 기능을 구현함
    
    * `handleSubmit`은 먼저 사용자가 작성한 데이터를 firebase의 실시간 데이터베이스에 등록함. 이때 등록되는 데이터베이스의 이름을 `channels`로 지정하고 id(고유 키값), name,details(사용자가 작성한 데이터)를 `newChannel`이라는 객체로 감싸서  updates라는 빈 객체에 [`channels`,고유키] 항목에 `newChannel`을 추가시킴
    
    * `update`메서드를 호출해서 firebase 데이터베이스에 `updates`를 추가하고, 사용자가 작성했던 값을 초기화 시킨뒤 `Dialog`가 꺼지게 설정함
    
    
    * 데이터베이스에 추가될 때 `channels` 리스트에 이전의 데이터와 함께 추가된 데이터를 추가해줘서 페이지에 렌더링 될수 있도록 작성함
    
    * `channels`의 길이만큼 `ListItem`을 반복 렌더링하고 이때의 key값은 `channel.id`값으로 지정함

--- src/components/ChannelMenu.jsx
+++ src/components/ChannelMenu.jsx
@@ -12,15 +12,53 @@ import {
   ListItemText,
   TextField,
 } from '@mui/material';
-import React, {useState} from 'react';
+import React, {useCallback, useState} from 'react';
 import AddIcon from '@mui/icons-material/Add';
+import "../firebase"
+import { child, getDatabase, onChildAdded, push, ref, update } from 'firebase/database';
+import { useEffect } from 'react';
 
 function ChannelMenu() {
   const [open, setOpen] = useState(false);
   const [channelName, setChannelName] = useState('');
   const [channelDetail, setChannelDetail] = useState('');
+  const [channels, setChannels] = useState([]);
   const handleClickOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+
+  useEffect(() => {
+    const db = getDatabase();
+    const unsubscribe = onChildAdded(ref(db, "channels"), (snapshot) => {
+      setChannels((channelArr) =>[...channelArr, snapshot.val()] )
+    })
+
+    return () => {
+      setChannels([])
+      unsubscribe()
+    }
+  },[])
+
+  const handleSubmit = useCallback(async () => {
+    //firebase의 데이터베이스에 데이터를 등록
+    const db = getDatabase();
+    const key = push(child(ref(db), "channels")).key;
+    const newChannel = {
+      id: key,
+      name: channelName,
+      details: channelDetail
+    };
+    const updates = {};
+    updates["/channels/" + key] = newChannel
+
+    try {
+      await update(ref(db), updates)
+      setChannelName("")
+      setChannelDetail("")
+      handleClose()
+    } catch (error) {
+      console.error(error)
+    }
+  },[channelDetail, channelName])
 
   return (
     <>
@@ -37,6 +75,12 @@ function ChannelMenu() {
             <ListItemText primary="채널" sx={{wordBreak: 'break-all', color: '#9a939b'}} />
           </ListItemIcon>
         </ListItem>
+        {channels.map((channel) => (
+          // TODO store 구현 , selected 구현
+          <ListItem button key={channel.id}>
+            <ListItemText primary={`# ${channel.name}`} sx={{wordBreak: 'break-all', color: '#9a939b'}} />
+          </ListItem>
+        ))}
       </List>
       <Dialog open={open} onClose={handleClose}>
         <DialogTitle>채널 추가</DialogTitle>
@@ -62,7 +106,7 @@ function ChannelMenu() {
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>취소</Button>
-          <Button>생성</Button>
+          <Button onClick={handleSubmit}>생성</Button>
         </DialogActions>
       </Dialog>
     </>
