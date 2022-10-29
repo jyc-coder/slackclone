b7728da8f743438908ffd6e5fae140d78ae17077
jyc-coder <jyc4648@gmail.com>
2022-10-29 23:04:52 +0900

    #15 - 채널 메뉴 기능 구현 (선택 채널 관리)
    * 사용자가 처음으로 페이지로 이동할때는 첫 번째의 채널이 선택된 상태가 되도록 useEffect를 통해 설정함
    
    * 만약 생성된 채널중에 하나를 클릭하여 선택할 때 `changeChannel` 메서드가 호출되게 설정함. 
    `changeChannel`은 클릭한 채널의 id값을 `ActiveChannelId`의 값과 동일하게 만들어줌
    
    `selected`의 기준은 `channel.id`와 `activeChannelId`의 값이 동일한 경우에 적용되기 때문에 클릭한 채널이 `selected`상태로 변함
    
    * 컴포넌트 내부 뿐만아니라 전역에서 상태 변화를 파악하기 위해서 redux를 활용하여 store 내부에 `channelReducer`를 작성함
    
    * `channelReducer` 는 `CurrentChannel`의 값을 변경하는 `setCurrentChannel`액션 생성함수만 보유하고 있음
    
    * 사용자가 다른 채널을 클릭할때마다 , 혹은 새로고침을 할때(무조건 첫번째 채널이 선택되게 설정했기 때문)`setCurrentChannel`을 dispatch 해서 `currentChannel`이 변동되는 것을 파악함
    
    * redux사용을 위해 `redux-devtools-extension`설치 및 index에 `devTool`사용 코드 추가함

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 
 import { combineReducers } from "redux";
+import channelReducer from "./channelReducer";
 import userReducer from './userReducer';
 
 const rootReducer = combineReducers({
     user: userReducer,
+    channel: channelReducer
 })
 
 
