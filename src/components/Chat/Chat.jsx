62dd2a291326150894c6ebb0834db4e5c1411ea9
jyc-coder <jyc4648@gmail.com>
2022-10-31 10:40:03 +0900

    #17 - 메시지 영역 UI 구현
    * 메시지 영역 UI를 구현한 `Chat` 컴포넌트를 추가함
     
    * `ChatHeader`와 `Grid` 컴포넌트로 구성되어있으며 `Grid`내부에는 `ChatInput`컴포넌트가 들어있으며 TextField 컴포넌트로 채팅을 입력하는 영역임
    
    * `ChatHeader`는 redux에서 관리하는 state에서 가져온 `currentChannel`을 props로 받아와서 채널의 이름(name) 과 자세한 설명(detail) 데이터를 표시함
    
    *`ChatInput` 은 `TextFiled`컴포넌트를 사용했으며 `InputProps`를 추가해서 좌측에는 이모티콘과 이미지를 삽입하는 기능을 뜻하는 `IconButton`을 추가했으며 우측에는 메시지 전송을 뜻하는 `SendIcon`을 추가함

---
+++ src/components/Chat/Chat.jsx
@@ -0,0 +1,24 @@
+import { Divider, Grid, List, Paper, Toolbar } from '@mui/material'
+import React from 'react'
+import ChatHeader from './ChatHeader'
+import { useSelector } from 'react-redux';
+import ChatInput from './ChatInput';
+
+function Chat() {
+     const {channel} = useSelector((state) => state);
+  return (
+    <>
+      <Toolbar />
+          <ChatHeader channelInfo={channel.currentChannel} />
+          <Grid container component={Paper} variant="outlined" sx={{ mt: 3, position: "relative" }}>
+              <List sx={{ height: "calc(100vh - 350px)", overflow: "scroll", width: "100%", position: "relative" }}> 
+                  {/* 채팅메세지 */}
+              </List>
+              <Divider />
+              <ChatInput/>
+          </Grid>
+    </>
+  );
+}
+
+export default Chat
