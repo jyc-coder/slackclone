018316abedad50a1d2b55018a24af0d9e1cc4013
jyc-coder <jyc4648@gmail.com>
2022-10-31 15:37:18 +0900

    #21 - 채팅 메시지 입력 기능 구현
    * 사용자가 채팅 메시지를 입력하고 `sendIcon`을 클릭하면 `clickSendMessage`를 호출하여 firebase의 데이터베이스에 데이터가 등록됨
    
    * firebase 데이터베이스에 등록되는 데이터의 형태는 `currentChannel.id`에 `timestamp`(메시지를 보낸 시간),`user`(id, name, avatar),`content`(사용자가 입력한 메시지) 으로 구성됨

--- src/components/Chat/ChatInput.jsx
+++ src/components/Chat/ChatInput.jsx
@@ -1,42 +1,74 @@
-import { InsertEmoticon } from '@mui/icons-material'
-import { Grid, IconButton, InputAdornment, TextField } from '@mui/material'
-import React from 'react'
+import {InsertEmoticon} from '@mui/icons-material';
+import {Grid, IconButton, InputAdornment, TextField} from '@mui/material';
+import React, {useCallback, useState} from 'react';
 import ImageIcon from '@mui/icons-material/Image';
 import SendIcon from '@mui/icons-material/Send';
+import '../../firebase';
+import {getDatabase, push, ref, serverTimestamp, set} from 'firebase/database';
+import {useSelector} from 'react-redux';
 function ChatInput() {
+  const {channel, user} = useSelector((state) => state);
+  const [message, setMessage] = useState('');
+  const [loading, setLoading] = useState(false);
+  const handleChange = useCallback((e) => {
+    setMessage(e.target.value);
+  }, []);
+
+  const createMessage = useCallback(() => ({
+    timestamp: serverTimestamp(),
+      user: { id: user.currentUser.uid, name: user.currentUser.displayName, avatar: user.currentUser.photoURL },
+    content:message
+  }), [message, user.currentUser.displayName, user.currentUser.photoURL, user.currentUser.uid]);
+    
+    
+  const clickSendMessage = useCallback(async () => {
+    if (!message) return;
+    setLoading(true);
+    try {
+        await set(
+            push(ref(getDatabase(), 'messages/' + channel.currentChannel.id)),
+            createMessage()
+        );
+        setLoading(false);
+        setMessage("")
+    } catch (error) {
+        console.error(error)
+        setLoading(false)
+    }
+  }, [channel.currentChannel?.id, createMessage, message]);
+    
+    
   return (
-      <Grid container sx={{ p: "20px" }}>
-          <Grid item xs={12} sx={{ position: 'relative' }}>
-              <TextField
-                  InputProps={{
-                      startAdornment: (
-                          <InputAdornment position="start">
-                              <IconButton>
-                                  <InsertEmoticon/>
-                              </IconButton>
-                              <IconButton>
-                                  <ImageIcon/>
-                              </IconButton>
-                          </InputAdornment>
-                      ),
-                      endAdornment: (
-                          <InputAdornment positon="start">
-                              <IconButton>
-                                  <SendIcon/>
-                              </IconButton>
-                          </InputAdornment>
-                      )
-                  }}
-                  autoComplete="off"
-                  label="메세지 입력"
-                  fullWidth
-              >
-                  
-              </TextField>
-          </Grid>
-          
+    <Grid container sx={{p: '20px'}}>
+      <Grid item xs={12} sx={{position: 'relative'}}>
+        <TextField
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <IconButton>
+                  <InsertEmoticon />
+                </IconButton>
+                <IconButton>
+                  <ImageIcon />
+                </IconButton>
+              </InputAdornment>
+            ),
+            endAdornment: (
+              <InputAdornment position="start">
+                <IconButton disabled={loading} onClick={clickSendMessage}>
+                  <SendIcon/>
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+          autoComplete="off"
+          label="메세지 입력"
+          fullWidth
+          onChange={handleChange}
+        ></TextField>
+      </Grid>
     </Grid>
-  )
+  );
 }
 
-export default ChatInput
+export default ChatInput;
