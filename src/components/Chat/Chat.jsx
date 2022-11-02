20482208dc4a3e9cc3fa1f40c44d4e551be762f1
jyc-coder <jyc4648@gmail.com>
2022-11-02 09:14:09 +0900

    #21 - 채팅 메시지 데이터 가져오는 기능 구현
    * firebase 데이터베이스에 등록된 데이터를 가져와서 `ChatMessage`의 props로 전달하여 작성자, 아바타, 메시지 내용을 전달하게 설정함
    
    * useEffect로 fireBase에 있는 메시지 데이터를 불러와서 `message`리스트에 추가하여 메시지들이 렌더링 될수 있게 설정함
    
    * `query`를 통해서 `timestamp`를 기준으로 메시지 데이터를 정렬하고, 채팅 메시지가 추가될 때마다 `setMessages`를 통해 최근 추가된 메시지를 `message`리스트에 추가함으로써 사용자가 최근에 입력한 메시지도 바로 렌더링되어 보이게끔 설정함

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,19 +1,49 @@
 import { Divider, Grid, List, Paper, Toolbar } from '@mui/material'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ChatHeader from './ChatHeader'
 import { useSelector } from 'react-redux';
 import ChatInput from './ChatInput';
 import ChatMessage from './ChatMessage';
+import "../../firebase"
+import { child, get, getDatabase, onChildAdded, orderByChild, query, ref, startAt } from 'firebase/database';
 
 function Chat() {
-     const {channel} = useSelector((state) => state);
+  const { channel, user } = useSelector((state) => state);
+
+  const [messages, setMessages] = useState([])
+  
+
+  useEffect(() => {
+    if (!channel.currentChannel || !user.currentUser) return;
+    async function getMessages() {
+      const snapShot = await get(child(ref(getDatabase()), "messages/" + channel.currentChannel.id));
+      setMessages(snapShot.val() ? Object.values(snapShot.val()) : []);
+    }
+    getMessages();
+    return () => {
+      setMessages([])
+    }
+  }, [channel.currentChannel, user.currentUser])
+  
+  useEffect(() => {
+    if (!channel.currentChannel || !user.currentUser) return;
+    const sorted = query(ref(getDatabase(), "messages/" + channel.currentChannel.id), orderByChild("timestamp"));
+    const unsubscribe = onChildAdded(query(sorted, startAt(Date.now())), (snapshot) => setMessages((oldMessages) => [...oldMessages, snapshot.val()]))
+    
+    return () => {
+      unsubscribe?.();
+    }
+  },[channel.currentChannel, user.currentUser])
+
   return (
     <>
       <Toolbar />
           <ChatHeader channelInfo={channel.currentChannel} />
           <Grid container component={Paper} variant="outlined" sx={{ mt: 3, position: "relative" }}>
               <List sx={{ height: "calc(100vh - 350px)", overflow: "scroll", width: "100%", position: "relative" }}> 
-                  <ChatMessage/>
+          {messages.map(message => (
+            <ChatMessage key={message.timestamp} message={message} user={user} />
+                 ))}
               </List>
               <Divider />
               <ChatInput/>
