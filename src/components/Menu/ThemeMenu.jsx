c138a1dd860743788c57d1505e64eeafb576488a
jyc-coder <jyc4648@gmail.com>
2022-11-04 14:59:50 +0900

    #33 - 테마 색상 firebase 연동
     사용자가 색상을 선택하고 `테마 저장`을 클릭하면 `mainTheme`과 `녀ㅠ쏘듣`의 색상 정보를 firebase 데이터베이스에 저장하고 해당 데이터를 불러와서 테마 리스트를 렌더링함

--- src/components/Menu/ThemeMenu.jsx
+++ src/components/Menu/ThemeMenu.jsx
@@ -1,14 +1,60 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle, List, ListItem, ListItemIcon, Stack } from '@mui/material'
-import React, { useCallback, useState } from 'react'
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  List,
+  ListItem,
+  ListItemIcon,
+  Stack,
+} from '@mui/material';
+import React, {useCallback, useEffect, useState} from 'react';
 import PaletteIcon from '@mui/icons-material/Palette';
-import { HexColorPicker } from 'react-colorful';
-
+import {HexColorPicker} from 'react-colorful';
+import '../../firebase';
+import {child, getDatabase, onChildAdded, push, ref, update} from 'firebase/database';
+import {useSelector} from 'react-redux';
 
 function ThemeMenu() {
-    const [showThemeModal, setShowThemeModal] = useState(false)
+  const {user} = useSelector((state) => state);
+  const [showThemeModal, setShowThemeModal] = useState(false);
+  const [mainTheme, setMainTheme] = useState('#ffffff');
+    const [subTheme, setSubTheme] = useState('#ffffff');
+    const [userTheme, setUserTheme] = useState([]);
+  const handleClickOpen = useCallback(() => setShowThemeModal(true), []);
+  const handleClose = useCallback(() => setShowThemeModal(false), []);
+  const handleChangeMain = useCallback((color) => setMainTheme(color), []);
+  const handleChangeSub = useCallback((color) => setSubTheme(color), []);
 
-    const handleClickOpen = useCallback(() => setShowThemeModal(true),[]);
-    const handleClose = useCallback(() => setShowThemeModal(false),[]);
+    const handleSaveTheme = useCallback(async () => {
+        if (!user.currentUser?.uid) return;
+    try {
+      const db = getDatabase();
+      const key = push(child(ref(db), '/users/' + user.currentUser.uid + '/theme')).key;
+      const newTheme = {mainTheme, subTheme};
+      const updates = {};
+      updates['/users/' + user.currentUser.uid + '/theme/' + key] = newTheme;
+      await update(ref(db), updates);
+      handleClose();
+    } catch (error) {
+      console.error(error);
+      handleClose();
+    }
+    }, [handleClose, mainTheme, subTheme, user.currentUser.uid]);
+    
+    useEffect(() => {
+        if (!user.currentUser?.uid) return;
+        const db = getDatabase();
+        const themeRef = ref(db, 'users/' + user.currentUser.uid + '/theme');
+        const unsubscribe = onChildAdded(themeRef, (snap) => {
+            setUserTheme((themeArr) => [snap.val(), ...themeArr])
+        }) 
+        return () => {
+            setUserTheme([])
+            unsubscribe?.();
+        }
+    },[user.currentUser.uid])
   return (
     <>
       <List sx={{overflow: 'auto', width: 60, backgroundColor: '#150c16'}}>
@@ -17,13 +63,15 @@ function ThemeMenu() {
             <PaletteIcon />
           </ListItemIcon>
         </ListItem>
-        <ListItem>
-          <div className="theme-box">
-            <div className="theme-main" style={{backgroundColor: 'red'}}></div>
+        {userTheme.map((theme,i) => (
+            <ListItem key={i}>
+            <div className="theme-box">
+              <div className="theme-main" style={{backgroundColor: theme.mainTheme}}></div>
 
-            <div className="theme-sub" style={{backgroundColor: 'white'}}></div>
-          </div>
-        </ListItem>
+              <div className="theme-sub" style={{backgroundColor: theme.subTheme}}></div>
+            </div>
+          </ListItem>
+        ))}
       </List>
       <Dialog open={showThemeModal} onClose={handleClose}>
         <DialogTitle>테마 색상 선택</DialogTitle>
@@ -31,21 +79,21 @@ function ThemeMenu() {
           <Stack direction="row" spacing={2}>
             <div>
               Main
-              <HexColorPicker />
+              <HexColorPicker color={mainTheme} onChange={handleChangeMain} />
             </div>
             <div>
               sub
-              <HexColorPicker />
+              <HexColorPicker color={subTheme} onChange={handleChangeSub} />
             </div>
           </Stack>
         </DialogContent>
         <DialogActions>
-                  <Button onClick={handleClose}>취소</Button>
-          <Button>테마 저장</Button>
+          <Button onClick={handleClose}>취소</Button>
+          <Button onClick={handleSaveTheme}>테마 저장</Button>
         </DialogActions>
       </Dialog>
     </>
   );
 }
 
-export default ThemeMenu
+export default ThemeMenu;
